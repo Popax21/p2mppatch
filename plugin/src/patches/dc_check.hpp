572663f1b67d596779ca779a53edec4baa83098e
Popax21 <popa21@popa21.at>
2023-06-12 00:38:18 +0200

    Fix disconnect check crash

--- plugin/src/patches/dc_check.hpp
+++ plugin/src/patches/dc_check.hpp
@@ -14,11 +14,11 @@ namespace patches {
             virtual void register_patches(CMPPatchPlugin& plugin) {
                 //Find CPortalMPGameRules::ClientDisconnected and remove disconnect check
                 SAnchor CPortalMPGameRules_ClientDisconnected = PATCH_FUNC_ANCHOR(plugin.server_module(), CPortalMPGameRules::ClientDisconnected);
-                plugin.register_patch<CPatch>(CPortalMPGameRules_ClientDisconnected + 0x82, new SEQ_HEX("8b 4c 24 0c"), new SEQ_HEX("66 e9 9d 00")); //Jump over the check
+                plugin.register_patch<CPatch>(CPortalMPGameRules_ClientDisconnected + 0x8d, new SEQ_HEX("74"), new SEQ_HEX("eb"));
 
                 //Find CPortal_Player::PlayerTransitionCompleteThink and remove disconnect check
                 SAnchor CPortal_Player_PlayerTransitionCompleteThink = PATCH_FUNC_ANCHOR(plugin.server_module(), CPortal_Player::PlayerTransitionCompleteThink);
-                plugin.register_patch<CPatch>(CPortal_Player_PlayerTransitionCompleteThink + 0x41, new SEQ_HEX("74 54"), new SEQ_HEX("eb 54")); //Patch the if check to always fail
+                plugin.register_patch<CPatch>(CPortal_Player_PlayerTransitionCompleteThink + 0x41, new SEQ_HEX("74 54"), new SEQ_HEX("eb 54"));
             }
     };
 };
